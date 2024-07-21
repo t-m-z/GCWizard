0aa2b4449fc27c7f9bd30b381adc821d6467d6dc
THOMAS-DESKTOP\thoma <thomas@familiezimmermann.de>
2024-07-21 14:34:09 +0200

    fix errors

--- lib/tools/images_and_files/adventure_labs/widget/adventure_labs.dart
+++ lib/tools/images_and_files/adventure_labs/widget/adventure_labs.dart
@@ -7,7 +7,7 @@ import 'package:gc_wizard/common_widgets/buttons/gcw_button.dart';
 import 'package:gc_wizard/common_widgets/buttons/gcw_iconbutton.dart';
 import 'package:gc_wizard/common_widgets/dividers/gcw_text_divider.dart';
 import 'package:gc_wizard/common_widgets/gcw_expandable.dart';
-import 'package:gc_wizard/common_widgets/gcw_tool.dart';
+import 'package:gc_wizard/application/tools/widget/gcw_tool.dart';
 import 'package:gc_wizard/common_widgets/outputs/gcw_columned_multiline_output.dart';
 import 'package:gc_wizard/common_widgets/outputs/gcw_output_text.dart';
 import 'package:gc_wizard/common_widgets/spinners/gcw_dropdown_spinner.dart';
