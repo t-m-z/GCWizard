7ddf04161f40050514bc27887a57b1951449475d
tmz <thomas@familiezimmermann.de>
2025-12-19 21:18:49 +0100

    refactor add async executer

---
+++ lib/tools/coords/rescue_points/logic/rescue_points_map.dart
@@ -0,0 +1,96 @@
+import 'dart:convert';
+import 'dart:isolate';
+import 'package:gc_wizard/application/theme/fixed_colors.dart';
+import 'package:gc_wizard/common_widgets/async_executer/gcw_async_executer_parameters.dart';
+import 'package:gc_wizard/tools/coords/_common/logic/coordinates.dart';
+import 'package:gc_wizard/tools/coords/map_view/logic/map_geometries.dart';
+import 'package:flutter/services.dart';
+import 'package:gc_wizard/tools/coords/_common/logic/ellipsoid.dart';
+import 'package:gc_wizard/tools/coords/distance_and_bearing/logic/distance_and_bearing.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:xml/xml.dart';
+
+class RescuePointJobData {
+  final BaseCoordinate jobDataCenter;
+  final int jobDataRadius;
+  final String jobDataFilename;
+
+  RescuePointJobData({
+    required this.jobDataCenter,
+    required this.jobDataRadius,
+    required this.jobDataFilename,
+  });
+}
+
+Future<List<GCWMapPoint>> getRescuePointsAsync(GCWAsyncExecuterParameters? jobData) async {
+  if (jobData?.parameters is! RescuePointJobData) return Future.value([]);
+  var data = jobData!.parameters as RescuePointJobData;
+  var output = await _getRescuePoints(data.jobDataCenter, data.jobDataRadius, data.jobDataFilename, sendAsyncPort: jobData.sendAsyncPort);
+
+  jobData.sendAsyncPort?.send(output);
+  return output;
+}
+
+Future<List<GCWMapPoint>> _getRescuePoints(BaseCoordinate center, int radius, String filename, {SendPort? sendAsyncPort}) async {
+
+  final bytes = await rootBundle.load(filename);
+
+  return _importRescuePointFile(bytes.buffer.asUint8List(), center.toLatLng()!, radius);
+}
+
+bool _inRange(LatLng coord1, LatLng coord2, int distance) {
+  return (distanceBearing(coord1, coord2, Ellipsoid.WGS84).distance <=
+      distance);
+}
+
+List<GCWMapPoint> _importRescuePointFile(Uint8List bytes, LatLng center, int radius)  {
+  var xml = utf8.decode(bytes);
+  return _parseRescuePointFile(xml, center, radius);
+}
+
+List<GCWMapPoint> _parseRescuePointFile(String xml, LatLng center, int radius) {
+  List<GCWMapPoint> result;
+  var xmlDoc = XmlDocument.parse(xml);
+  result = _GpxReader()._parse(xmlDoc, center, radius);
+  return result;
+}
+
+/// Convert RescuePoints GPX-XML into points
+class _GpxReader {
+  List<GCWMapPoint> _parse(XmlDocument xmlDocument, LatLng center, int radius) {
+    var parent = xmlDocument.getElement('gpx');
+    if (parent != null) {
+      var points = <GCWMapPoint>[];
+
+      parent.findAllElements('wpt').forEach((xmlWpt) {
+        var wpt = _readPoint(xmlWpt, center, radius);
+        if (wpt != null) points.add(wpt);
+      });
+
+      return points;
+    }
+    return [];
+  }
+
+  GCWMapPoint? _readPoint(XmlElement xmlElement, LatLng center, int radius) {
+    var lat = xmlElement.getAttribute('lat');
+    var lon = xmlElement.getAttribute('lon');
+
+    if (lat != null && lon != null) {
+      if (_inRange(LatLng(double.parse(lat), double.parse(lon)), center, radius)) {
+        var wpt = GCWMapPoint(
+            point: LatLng(double.tryParse(lat) ?? 0, double.tryParse(lon) ?? 0),
+            isEditable: true);
+        var name = xmlElement.getElement('name')?.innerText ?? '';
+        var desc = xmlElement.getElement('desc')?.innerText ?? '';
+        var src = xmlElement.getElement('src')?.innerText ?? '';
+
+        wpt.markerText = name + '\n' +  desc + '\n' + src;
+        wpt.color = COLOR_MAP_GPX_IMPORT_WAYPOINT;
+
+        return wpt;
+      }
+    }
+    return null;
+  }
+}
--- lib/tools/coords/rescue_points/widget/rescue_points_map.dart
+++ lib/tools/coords/rescue_points/widget/rescue_points_map.dart
@@ -1,22 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:gc_wizard/application/i18n/logic/app_localizations.dart';
+import 'package:gc_wizard/common_widgets/async_executer/gcw_async_executer.dart';
+import 'package:gc_wizard/common_widgets/async_executer/gcw_async_executer_parameters.dart';
 import 'package:gc_wizard/common_widgets/buttons/gcw_button.dart';
 import 'package:gc_wizard/common_widgets/buttons/gcw_submit_button.dart';
+import 'package:gc_wizard/common_widgets/gcw_snackbar.dart';
 import 'package:gc_wizard/common_widgets/outputs/gcw_columned_multiline_output.dart';
 import 'package:gc_wizard/common_widgets/outputs/gcw_default_output.dart';
 import 'package:gc_wizard/common_widgets/spinners/gcw_integer_spinner.dart';
-import 'package:gc_wizard/tools/coords/_common/logic/coordinate_format.dart';
 import 'package:gc_wizard/tools/coords/_common/logic/default_coord_getter.dart';
-import 'package:gc_wizard/tools/coords/_common/logic/ellipsoid.dart';
-import 'package:gc_wizard/tools/coords/_common/logic/gpx_kml_gpx_import.dart';
 import 'package:gc_wizard/tools/coords/_common/widget/gcw_coords.dart';
-import 'package:gc_wizard/tools/coords/distance_and_bearing/logic/distance_and_bearing.dart';
 import 'package:gc_wizard/tools/coords/map_view/logic/map_geometries.dart';
-import 'package:gc_wizard/tools/coords/map_view/persistence/model.dart';
 import 'package:gc_wizard/tools/coords/map_view/widget/gcw_mapview.dart';
-import 'package:gc_wizard/utils/file_utils/gcw_file.dart';
-import 'package:gc_wizard/utils/ui_dependent_utils/color_utils.dart';
-import 'package:latlong2/latlong.dart';
+import 'package:gc_wizard/tools/coords/rescue_points/logic/rescue_points_map.dart';
 
 class RescuePointsMapView extends StatefulWidget {
   RescuePointsMapView({super.key}) : super();
@@ -36,47 +33,6 @@ class _MapViewState extends State<RescuePointsMapView> {
   final String _ASSET_PATH =
       'lib/tools/coords/rescue_points/assets/GPX_KWF_RP.gpx';
 
-  bool _inRange(LatLng coord1, LatLng coord2, int distance) {
-    return (distanceBearing(coord1, coord2, Ellipsoid.WGS84).distance <=
-        distance);
-  }
-
-  void _loadCoordinatesFile() async {
-    // Read the Zip file from disk.
-    final bytes = await DefaultAssetBundle.of(context).load(_ASSET_PATH);
-
-    GCWFile file =
-        GCWFile(name: _ASSET_PATH, bytes: bytes.buffer.asUint8List());
-
-    await importCoordinatesFile(file).then((viewData) {
-      if (viewData != null) {
-        int i = 0;
-        for (MapPointDAO point in viewData.points) {
-          if (_inRange(LatLng(point.latitude, point.longitude),
-              _currentCoords.toLatLng()!, _currentRange)) {
-            points.add(GCWMapPoint(
-                uuid: point.uuid,
-                markerText: point.name,
-                point: LatLng(point.latitude, point.longitude),
-                coordinateFormat:
-                    CoordinateFormat.fromPersistenceKey(point.coordinateFormat),
-                isVisible: point.isVisible,
-                color: hexStringToColor(point.color),
-                circle: point.radius != null
-                    ? GCWMapCircle(
-                        centerPoint: LatLng(point.latitude, point.longitude),
-                        radius: point.radius!,
-                        color:
-                            hexStringToColor(point.circleColor ?? point.color))
-                    : null,
-                circleColorSameAsPointColor: point.circleColorSameAsColor,
-                isEditable: point.isEditable ?? false));
-          }
-        }
-      }
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -95,7 +51,7 @@ class _MapViewState extends State<RescuePointsMapView> {
         GCWIntegerSpinner(
           title: i18n(context, 'common_radius'),
           min: 0,
-          max: 2000,
+          max: 3000,
           value: _currentRange,
           onChanged: (value) {
             setState(() {
@@ -106,7 +62,7 @@ class _MapViewState extends State<RescuePointsMapView> {
         GCWSubmitButton(
           onPressed: () {
             setState(() {
-              _loadCoordinatesFile();
+              _getRescuePointsAsync();
             });
           },
         ),
@@ -120,7 +76,7 @@ class _MapViewState extends State<RescuePointsMapView> {
     for (var point in points) {
       result.add([
         point.markerText.toString(),
-        point.point.latitude.toString().padRight(10, '0').substring(0, 10),
+        point.point.latitude.toString().padRight(10, '0').substring(0, 10) + '\n' +
         point.point.longitude.toString().padRight(10, '0').substring(0, 10),
       ]);
     }
@@ -132,7 +88,7 @@ class _MapViewState extends State<RescuePointsMapView> {
         child: Column(children: [
       GCWColumnedMultilineOutput(
         data: _pointsToList(),
-        flexValues: [2, 3, 3],
+        flexValues: [8, 3],
       ),
       GCWButton(
         text: i18n(context, 'coords_show_on_map'),
@@ -142,4 +98,47 @@ class _MapViewState extends State<RescuePointsMapView> {
       ),
     ]));
   }
+
+  void _getRescuePointsAsync() async {
+    await showDialog<bool>(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return Center(
+          child: SizedBox(
+            height: GCW_ASYNC_EXECUTER_INDICATOR_HEIGHT,
+            width: GCW_ASYNC_EXECUTER_INDICATOR_WIDTH,
+            child: GCWAsyncExecuter<List<GCWMapPoint>>(
+              isolatedFunction: getRescuePointsAsync,
+              parameter: _buildRescuePointsJobData,
+              onReady: (data) => _showRescuePointsOutput(data),
+              isOverlay: true,
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Future<GCWAsyncExecuterParameters?> _buildRescuePointsJobData() async {
+    return GCWAsyncExecuterParameters(RescuePointJobData(
+        jobDataCenter: _currentCoords,
+        jobDataRadius: _currentRange,
+        jobDataFilename: _ASSET_PATH,
+    ));
+  }
+
+  void _showRescuePointsOutput(List<GCWMapPoint> output) {
+
+    points = output;
+
+    SchedulerBinding.instance.addPostFrameCallback((_) {
+      showSnackBar(i18n(context, 'rescue_points_message'), context);
+    });
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {});
+    });
+  }
+
 }
