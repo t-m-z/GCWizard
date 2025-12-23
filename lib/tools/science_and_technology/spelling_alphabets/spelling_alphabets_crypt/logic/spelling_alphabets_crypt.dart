45294aaabf8c95cc47401c782e73b2a2e5c3b7c7
Thomas <thomas@familiezimmermann.de>
2025-12-23 16:18:21 +0100

    correct map, logic (#2389)
    add tests

--- lib/tools/science_and_technology/spelling_alphabets/spelling_alphabets_crypt/logic/spelling_alphabets_crypt.dart
+++ lib/tools/science_and_technology/spelling_alphabets/spelling_alphabets_crypt/logic/spelling_alphabets_crypt.dart
@@ -4,15 +4,42 @@ import 'package:gc_wizard/tools/crypto_and_encodings/substitution/logic/substitu
 import 'package:gc_wizard/tools/science_and_technology/spelling_alphabets/_common/spelling_alphabets_data.dart';
 
 String encodeSpellingAlphabets(String plain, SPELLING language) {
-  List<String> result = [];
-  var _alphabet = SPELLING_ALPHABETS[language]!;
-  var alphabet =  Map.fromEntries(_alphabet);
 
   if (plain.isEmpty) return '';
+
+  List<String> result = [];
+  var _alphabet = SPELLING_ALPHABETS[language]!;
+  Map<String, String> alphabet = {};
+  Map<String, String> alphabetLow = {};
+  switch (language) {
+    case SPELLING.GRC:
+      for (var entry in _alphabet) {
+        alphabet[entry.key.toUpperCase().split(',')[0]] =
+            entry.value.toUpperCase();
+        alphabetLow[entry.key.toUpperCase().split(',')[1]] =
+            entry.value.toUpperCase();
+      }
+      break;
+    case SPELLING.GRCLAT:
+      for (var entry in _alphabet) {
+        alphabet[entry.key.toUpperCase().split(',')[0]] =
+            entry.value.toUpperCase();
+        alphabetLow[entry.key.toUpperCase().split(',')[1]] =
+            entry.value.toUpperCase();
+      }
+      break;
+    default:
+      alphabet = Map.fromEntries(_alphabet);
+  }
 
   plain.toUpperCase().split('').forEach((letter) {
     if (alphabet[letter] != null) {
       result.add(alphabet[letter]!.toUpperCase());
+    }
+    if (language == SPELLING.GRCLAT || language == SPELLING.GRC) {
+      if (alphabetLow[letter] != null) {
+        result.add(alphabetLow[letter]!.toUpperCase());
+      }
     }
   });
   return result.join(' ');
@@ -24,7 +51,7 @@ String decodeSpellingAlphabets(String chiffre, SPELLING language) {
   if (chiffre.isEmpty) return '';
 
   for (var entry in SPELLING_ALPHABETS[language]!) {
-    alphabet[entry.value.toUpperCase()] = entry.key;
+    alphabet[entry.value.toUpperCase()] = entry.key.split(',')[0];
   }
 
   return substitution(chiffre.toUpperCase(), alphabet, caseSensitive: false).replaceAll(' ', '');
